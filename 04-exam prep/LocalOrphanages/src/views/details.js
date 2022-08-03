95e6c0c7f3ceff42464a8251393a5ff262dc07d6
Aleksandar <107432011+a2Aleksandrov@users.noreply.github.com>
2022-08-03 14:13:25 +0300

    updated donation button functionality

--- 04-exam prep/LocalOrphanages/src/views/details.js
+++ 04-exam prep/LocalOrphanages/src/views/details.js
@@ -1,8 +1,8 @@
-import { deletePost, getPostDetails } from '../data.js';
+import { deletePost, getPostDetails, makeDonation, showUserDonations, totalDonations } from '../data.js';
 import { html } from '../lib.js';
 import { getUserData } from '../util.js';
 
-const detailsTemplate = (post, isOwner, canDonate, onDelete, onDonate) => html`
+const detailsTemplate = (post, isOwner, canDonate, onDelete, onDonate, total, userDonations) => html`
 <section id="details-page">
     <h1 class="title">Post Details</h1>
 
@@ -16,7 +16,7 @@ const detailsTemplate = (post, isOwner, canDonate, onDelete, onDonate) => html`
                 <p class="post-description">Description: ${post.description}</p>
                 <p class="post-address">Address: ${post.address}</p>
                 <p class="post-number">Phone number: ${post.phone}</p>
-                <p class="donate-Item">Donate Materials: 0</p>
+                <p class="donate-Item">Donate Materials: ${total}</p>
                 <div class="btns">
                     ${isOwner ? html`
                     <a href="/edit/${post._id}" class="edit-btn btn">Edit</a>
@@ -33,7 +33,6 @@ const detailsTemplate = (post, isOwner, canDonate, onDelete, onDonate) => html`
 
 export async function detailsPage(ctx) {
     
-
     const post = await getPostDetails(ctx.params.id);
     const userData = getUserData();
     let isOwner = false;
@@ -41,10 +40,14 @@ export async function detailsPage(ctx) {
     if (userData && userData.id == post._ownerId) {
         isOwner = true;
     }
-    if (userData && userData.id != post._ownerId) {
+    let total = await totalDonations(ctx.params.id);
+    let userDonations = await showUserDonations(ctx.params.id);
+
+    if (userData && userDonations == 0 && userData.id != post._ownerId) {
         canDonate = true;
     }
-    ctx.render(detailsTemplate(post, isOwner, canDonate, onDelete,));
+
+    ctx.render(detailsTemplate(post, isOwner, canDonate, onDelete, onDonate, total, userDonations));
 
     async function onDelete() {
         let isConfirmed = confirm('Are you sure you want to delete this post ?');
@@ -52,6 +55,19 @@ export async function detailsPage(ctx) {
             await deletePost(ctx.params.id);
             ctx.page.redirect('/dashboard');
         }
+    }
+
+    async function onDonate() {
+        await makeDonation(ctx.params.id);
+
+        total = await totalDonations(ctx.params.id);
+        userDonations = await showUserDonations(ctx.params.id);
+
+        if (userDonations > 0) {
+            canDonate = false;
+        }
+    
+        ctx.render(detailsTemplate(post, isOwner, canDonate, onDelete, onDonate, total, userDonations));
     }
 } 
 
