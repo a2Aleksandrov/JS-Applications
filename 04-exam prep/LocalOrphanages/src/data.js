a9b9aa07526cfab266316afdeaa64a3771772459
Aleksandar <107432011+a2Aleksandrov@users.noreply.github.com>
2022-08-03 14:14:34 +0300

    updated donation button functionality

--- 04-exam prep/LocalOrphanages/src/data.js
+++ 04-exam prep/LocalOrphanages/src/data.js
@@ -30,3 +30,20 @@ export async function getUserPosts() {
     const userData = getUserData();
     return await get(`/data/posts?where=_ownerId%3D%22${userData.id}%22&sortBy=_createdOn%20desc`);
 }
+
+export async function makeDonation(postId) {
+    const donation = {
+        postId
+    }
+    return await post('/data/donations', donation);
+}
+
+export async function totalDonations(postId) {
+
+    return await get(`/data/donations?where=postId%3D%22${postId}%22&distinct=_ownerId&count`);
+}
+
+export async function showUserDonations(postId) {
+    const userData = getUserData();
+    return await get(`/data/donations?where=postId%3D%22${postId}%22%20and%20_ownerId%3D%22${userData.id}%22&count`);
+}
